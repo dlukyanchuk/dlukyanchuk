95d27c7130e0bc8e8358b909f6c6b2e6e66547aa
dlukyanchuk <7291054@gmail.com>
2017-02-13 21:47:33 +0300

    commit in code

--- chapter_001/src/dlukyanchuk/Calculate.java
+++ chapter_001/src/dlukyanchuk/Calculate.java
@@ -1,5 +1,10 @@
 package dlukyanchuk;
-
+/**
+* Class Класс для вычиления арифметических операций
+* @author dlukyanchuk
+* @since 13.02.2017
+* @version 1 
+*/
 public class Calculate
 {
  public static void main(String[] args) {
