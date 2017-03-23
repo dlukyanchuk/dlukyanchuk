c44e94af37278ce49edf312f3d8bc4429c070b31
dlukyanchuk <7291054@gmail.com>
2017-03-23 20:17:49 +0300

    Add Turn

---
+++ chapter_001/src/main/java/job4j/arrays/BubbleSort.java
@@ -0,0 +1,28 @@
+package arrays;
+/**
+* Переворачивание массива.
+* @author dlukyanchuk
+* @since 19.03.2017
+* @version 1
+*/
+
+
+public class BubbleSort {
+/**
+* Пузырьковый метод для сортировки массива.
+* @param t - переменная "стакан" для подмены элементов массива
+* @return - результат метода
+*/
+	public int[] sort (int[] array) {
+		for (i = 0; i<array.length; i++) {
+			for (j = 0; j<array.length - i; j++) {
+				if (array[j] > array[j + 1]) {
+					int t = array[j];
+					array [j] = array[j+1];
+					array [j+1] = t;
+				}
+			}
+		}
+		return array[];	
+}
+}
---
+++ chapter_001/src/main/java/job4j/arrays/Turn.java
@@ -0,0 +1,22 @@
+package arrays;
+/**
+* Переворачивание массива.
+* @author dlukyanchuk
+* @since 19.03.2017
+* @version 1
+*/
+public class Turn {
+/**
+* Метод переворачивания массива.
+* @param index - номер элемента массива
+* @return - результат метода
+*/
+public int[] back(int[] array) {
+	for (int index = 0; index <= lenght.array / 2; index++) {
+		array [index] = array [lenght.array - index];
+			
+	}
+	return array[];
+}
+
+}
