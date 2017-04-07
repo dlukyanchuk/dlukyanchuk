87c1cad8ccbc49a2383c7729616e5e50a1bdf073
dlukyanchuk <7291054@gmail.com>
2017-04-07 22:04:40 +0300

    Add ArrayDublicate

---
+++ chapter_001/src/main/java/job4j/arrays/ArrayDuplicate.java
@@ -0,0 +1,28 @@
+package arrays;
+/**
+* Удаление дубликатов.
+* @author dlukyanchuk
+* @since 26.03.2017
+* @version 1
+*/
+public class RotateArray {
+/**
+* Метод для удаления дублированных строк.
+* @param array - данный массив
+* @return - результат метода
+*/
+	public String[] remove(String[] array) {
+		int copies = 0;
+			for (int i = 0; i < array.length - copies; i++) {
+				for (int j = i + 1; j < array.length - copies; j++) {
+					if (array[i].equals(array[j])) {
+						copies += 1;
+						String glass = array[j];
+						array[j] = array[array.length - copies];
+						array[array.length - copies] = glass;
+					}
+				}
+			}
+	return Arrays.copyOf(array, array.length - copies);
+}
+}
--- chapter_001/src/main/java/job4j/arrays/RotateArray.java
+++ chapter_001/src/main/java/job4j/arrays/RotateArray.java
@@ -17,10 +17,10 @@ public class RotateArray {
 		for (i = 0; i < array.length - 1; i++) {
 			for (j = 0; j < array.length - 1; j++) {
 				int glass = array[i][j];
-				array[i][j] = array[j][array.length-j];
-				array[j][array.length-j] = array[array.length-i][array.length-j];
-				array[array.length-i][array.length-j] = array[i][array.length-i];
-				array[i][array.length-i] = glass;
+				array[i][j] = array[j][array.length - j];
+				array[j][array.length - j] = array[array.length - i][array.length - j];
+				array[array.length - i][array.length - j] = array[i][array.length - i];
+				array[i][array.length - i] = glass;
 			}
 		}
 		return array;
